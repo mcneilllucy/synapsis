69e6b48325e49ae4551be86fa556c606930eb935
Lucy McNeill <lmcneill@svi.edu.au>
2021-06-16 11:47:33 +1000

    count_foci fully documented

--- R/count_foci.R
+++ R/count_foci.R
@@ -4,24 +4,19 @@
 #'
 #' @export count_foci
 #' @param img_path, path of crops
-#' @param stage, description
-#' @param offset_px, description
-#' @param offset_factor, description
-#' @param brush_size, description
-#' @param brush_sigma, description
-#' @param foci_norm, description
-#' @param annotate, description
+#' @param stage, meiosis stage of interest. Currently count_foci determines this with thresholding/ object properties in the dna channel. But will be classified using ML model in future versions.
+#' @param offset_px, Pixel value offset used in thresholding of dna channel
+#' @param offset_factor, Pixel value offset used in thresholding of foci channel
+#' @param brush_size, size of brush to smooth the foci channel. Should be small to avoid erasing foci.
+#' @param brush_sigma, sigma for Gaussian smooth of foci channel. Should be small to avoid erasing foci.
+#' @param foci_norm, Mean intensity to normalise all foci channels to.
+#' @param annotation, Choice to output pipeline choices (recommended to knit)
 
 #' @return foci count per cell
 
 
-count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_factor = 2, brush_size = 3, brush_sigma = 3, foci_norm = 0.01, annotate = "off")
+count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_factor = 2, brush_size = 3, brush_sigma = 3, foci_norm = 0.01, annotation = "off")
 {
-  # input :
-
-  # output : a bunch of output jpegs? Or save them all?
-
-  #BiocManager::install("EBImage")
   cell_count <- 0
   image_count <-0
   foci_counts <- 0
@@ -58,15 +53,12 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
       cell_count <- cell_count + 1
 
       new_img<-img_orig
-      #display(new_img)
-      #### now see which have the right amount of strands
       disc = makeBrush(21, "disc")
       disc = disc / sum(disc)
       localBackground = filter2(new_img, disc)
       offset = offset_px
       thresh_crop = (new_img - localBackground > offset)
       strands <- bwlabel(thresh_crop)
-      #display(strands)
       color_img_strands<- colorLabels(strands, normalize = TRUE)
       num_strands <- computeFeatures.shape(strands)
       num_strands <- data.frame(num_strands)
@@ -77,42 +69,24 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
       img_orig_foci <- img_orig_foci*mean_factor
 
       #### normalise the foci image
-
-
-      # Wayne: change to 2*bg
       offset = offset_factor*bg
-      #offset = 6*bg
       foci_th = foci_mask_crop > bg + offset
       ### smooth it
-      ### maybe up the contrast first??
       img_tmp_contrast = foci_mask_crop
-
-
-      #display(foci_mask_crop)
-      # Wayne: size = 1
       w = makeBrush(size = brush_size, shape = 'gaussian', sigma = brush_sigma)
       #w = makeBrush(size = 1, shape = 'gaussian', sigma = 3)
       img_flo = filter2(img_tmp_contrast, w)
-      ## only choose objects above bright pixel value
-      #foci_th = foci_mask_crop > 0.2
-      #foci_th = img_flo > 0.2
-      #display(img_flo)
-
       ## smooth foci channel
       foci_th = img_flo > bg + offset
-      #foci_th = img_flo > 0.05
-
-      #display(foci_th)
       foci_label = bwlabel(foci_th)
       foci_label <- channel(foci_label, "grey")
-      #display(colorLabels(strands))
       num_strands <- computeFeatures.shape(strands)
       num_strands <- data.frame(num_strands)
 
       ##### print properties of the images
       coincident_foci <- bwlabel(foci_label*strands)
       ### multiply strands by foci_label
-      if(annotate == "on"){
+      if(annotation == "on"){
         print("cell counter is")
         print(cell_count)
         print("original images")
@@ -121,7 +95,6 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
         print("displaying resulting foci count")
         print("Overlay two channels")
         display(rgbImage(strands,foci_label,0*foci_label))
-
         print("coincident foci")
         display(colorLabels(coincident_foci))
         print("two channels, only coincident foci")
@@ -131,7 +104,7 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
 
       overlap_no = table(coincident_foci)
       foci_per_cell <-  length(overlap_no)
-      if(annotate=="on"){
+      if(annotation=="on"){
         print("which counts this many foci:")
         print(foci_per_cell)
 
@@ -156,7 +129,7 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
       ### number of foci NOT on an SC
       alone_foci <- nrow(foci_candidates) - foci_per_cell
 
-      if(annotate == "on"){
+      if(annotation == "on"){
         print("number of alone foci")
         print(alone_foci)
         if(alone_foci < 0){
@@ -164,21 +137,14 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
           display(rgbImage(strands,foci_label,0*foci_label))
         }
       }
-
-      ####
-
       percent_px <- sum(overlap)/sum(foci_areas)
 
-      if(annotate == "on"){
+      if(annotation == "on"){
         print("percentage of foci channel coincident:")
         print(percent_px*100)
       }
-
-
       tryCatch({
-
         ### data frame stuff
-
         if(grepl( "++", file, fixed = TRUE) == TRUE){
           genotype <- "Fancm+/+"
         }
@@ -186,35 +152,19 @@ count_foci <- function(img_path, stage = "pachytene", offset_px = 0.2, offset_fa
         if(grepl( "--", file, fixed = TRUE) == TRUE){
           genotype <- "Fancm-/-"
         }
-
-
         ### data frame stuff ends
-
         df_cells <- rbind(df_cells,t(c(file,cell_count,genotype,stage,foci_per_cell, sd(foci_areas),mean(foci_areas),median(foci_areas),mean(image_mat),median(image_mat),percent_px,sd(image_mat),alone_foci)))
-
-
-
       },
       error = function(e) {
         #what should be done in case of exception?
         str(e) # #prints structure of exception
         print("couldn't crop it")
-
       }
       )
-
-
-
-
-
-      ###
     }
-
   }
   colnames(df_cells) <- df_cols
   return(df_cells)
-
-
 }
 
 
